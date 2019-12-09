914000e4de4dc8c8c4b3aaa1ec1f3a16d9393a99
Phalado <phalado@gmail.com>
2019-12-09 19:13:42 -0300

    Generated post model

---
+++ app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 140}
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_09_204538) do
+ActiveRecord::Schema.define(version: 2019_12_09_215609) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
